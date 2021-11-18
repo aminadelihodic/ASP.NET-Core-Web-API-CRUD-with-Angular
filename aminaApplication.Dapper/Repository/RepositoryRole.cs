256895ce09ceadbde8c693124e47c7b01112aed8
Amina <amina.djelihodjic@arni.ba>
2021-11-18 14:17:56 +0100

    change

--- aminaApplication.Dapper/Repository/RepositoryRole.cs
+++ aminaApplication.Dapper/Repository/RepositoryRole.cs
@@ -18,42 +18,7 @@ namespace aminaApplication.Dapper.Repository
 
         }
 
-        public void Delete(string id)
-        {
-            try
-            {
-                using (IDbConnection dbConnection = Connection)
-                {
-                    dbConnection.Open();
-                    string query = @"DELETE FROM roles WHERE id=@id";
-                    dbConnection.Execute(query, new { id });
-                    dbConnection.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-        }
-
-        public async Task<Role> GetById(string id)
-        {
-            try
-            {
-                using (IDbConnection dbConnection = Connection)
-                {
-                    dbConnection.Open();
-                    string query = @"SELECT id,description FROM roles WHERE id=@id";
-                    return await dbConnection.QueryFirstOrDefaultAsync<Role>(query, new { id });
-                }
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-        }
+        
     }
       
 }
